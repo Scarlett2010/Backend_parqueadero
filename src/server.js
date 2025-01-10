d5c896a056e5cc9318228cb08dda135ca7b3e04d
Scarlett Luna <scarlett.luna@epn.edu.ec>
2025-01-10 15:32:01 -0500

    2

--- src/server.js
+++ src/server.js
@@ -12,6 +12,10 @@ import swaggerUi from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 import { options } from "../swagger.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Inicializaciones
 const app = express();
