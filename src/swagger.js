86063045a0bacbe88bd393e1e70351f08b5d4667
Scarlett Luna <scarlett.luna@epn.edu.ec>
2025-01-10 14:58:22 -0500

    Actualizaciòn Swagger 2

--- src/swagger.js
+++ src/swagger.js
@@ -7,11 +7,6 @@ export const options = {
       description:
         "La API de Gestión de Lugares de Parqueo está diseñada para facilitar la administración eficiente de los espacios de estacionamiento en una instalación o área específica. Esta API permite a los administradores, guardias y usuarios interactuar con el sistema para obtener información en tiempo real sobre la disponibilidad de lugares de parqueo y realizar acciones relacionadas con la gestión de estos espacios.",
     },
-    servers: [
-      {
-        url: "https://backend-espacios-esfot.onrender.com/",
-      },
-    ],
   },
   apis: ["./src/routes/*.js"],
 };
